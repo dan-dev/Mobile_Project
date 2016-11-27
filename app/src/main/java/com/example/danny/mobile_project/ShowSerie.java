eccc85eee9d51d0c37ed3435957aad35d6739da5
dan-dev <danifermatos@gmail.com>
2016-11-27 23:53:42 +0000

    changed title to appbar

--- app/src/main/java/com/example/danny/mobile_project/ShowSerie.java
+++ app/src/main/java/com/example/danny/mobile_project/ShowSerie.java
@@ -14,7 +14,6 @@ public class ShowSerie extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_serie);
 
-        final TextView title = (TextView) findViewById(R.id.text_title);
         final TextView nEps = (TextView) findViewById(R.id.text_number_eps);
         final TextView descrip = (TextView) findViewById(R.id.text_description);
         final TextView actors = (TextView) findViewById(R.id.text_actors);
@@ -22,8 +21,8 @@ public class ShowSerie extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String ss = bundle.getString("title");
-        title.setText(ss);
 
+        getSupportActionBar().setTitle(ss);
         img.setImageResource(R.drawable.yeezus);
         descrip.setText(descrip.getText() + "Series about a God.");
         actors.setText(actors.getText() + "Kanye West, Yeezy, Drake");
