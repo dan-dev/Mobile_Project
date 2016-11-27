9348e048b3f51d0f52cbd9f4cd65018e8dd41f6a
dan-dev <danifermatos@gmail.com>
2016-11-27 23:09:12 +0000

    added listview for results and information on the series info page

--- app/src/main/java/com/example/danny/mobile_project/ResultActivity.java
+++ app/src/main/java/com/example/danny/mobile_project/ResultActivity.java
@@ -1,10 +1,18 @@
 package com.example.danny.mobile_project;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -13,13 +21,25 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        final TextView result = (TextView) findViewById(R.id.result);
-
+        final ListView listViewResults = (ListView) findViewById(R.id.listViewResults);
+        final Context cc = this;
         Bundle bundle = getIntent().getExtras();
         String ss = bundle.getString("search");
-        Log.d("log", ss);
-        result.setText(ss);
 
+        List<String> results = new ArrayList<String>();
+        results.add(ss);
 
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, results);
+
+        listViewResults.setAdapter(adapt);
+
+        listViewResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(cc, ShowSerie.class);
+                intent.putExtra("title", listViewResults.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/danny/mobile_project/ShowSerie.java
+++ app/src/main/java/com/example/danny/mobile_project/ShowSerie.java
@@ -2,6 +2,10 @@ package com.example.danny.mobile_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ShowSerie extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class ShowSerie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_serie);
+
+        final TextView title = (TextView) findViewById(R.id.text_title);
+        final TextView nEps = (TextView) findViewById(R.id.text_number_eps);
+        final TextView descrip = (TextView) findViewById(R.id.text_description);
+        final TextView actors = (TextView) findViewById(R.id.text_actors);
+        final ImageView img = (ImageView) findViewById(R.id.serie_image);
+
+        Bundle bundle = getIntent().getExtras();
+        String ss = bundle.getString("title");
+        title.setText(ss);
+
+        img.setImageResource(R.drawable.yeezus);
+        descrip.setText(descrip.getText() + "Series about a God.");
+        actors.setText(actors.getText() + "Kanye West, Yeezy, Drake");
+        nEps.setText(nEps.getText() + "24");
     }
 }
